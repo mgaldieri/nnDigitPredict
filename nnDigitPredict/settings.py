03ed7e0fc555275e72206745211a3e619377fa66
Tiago Barufi <barufi@gmail.com>
2013-06-28 11:15:48 -0300

    home is done

--- nnDigitPredict/settings.py
+++ nnDigitPredict/settings.py
@@ -1,4 +1,6 @@
 # Django settings for nnDigitPredict project.
+import os.path
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -61,7 +63,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/tiago/works/nndigitpredict/static' #os.path.join(os.path.dirname(__file__), 'static').replace('\\', '/')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -72,6 +74,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    #os.path.join(os.path.dirname(__file__), 'static').replace('\\', '/'),
 )
 
 # List of finder classes that know how to find static files in

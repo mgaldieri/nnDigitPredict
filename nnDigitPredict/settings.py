0dbb5fb8c087411a5c92f93af92b59e26b1c0516
Mauricio Galdieri <mgaldierigmail.com>
2013-06-28 13:58:16 -0300

    Static files settings.

--- nnDigitPredict/settings.py
+++ nnDigitPredict/settings.py
@@ -63,7 +63,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/home/mgaldieri/Developer/nndigitpredict/nnDigitPredict' #os.path.join(os.path.dirname(__file__), 'static').replace('\\', '/')
+# STATIC_ROOT = '/home/mgaldieri/Developer/nndigitpredict/nnDigitPredict' #os.path.join(os.path.dirname(__file__), 'static').replace('\\', '/')
+STATIC_ROOT = '/home/tiago/works/nndigitpredict/nnDigitPredict'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -71,7 +72,9 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    ("static", os.path.join(os.path.dirname(__file__), "static")),
+    os.path.join(os.path.dirname(__file__), "static"),
+    os.path.join(os.path.dirname(__file__), "static", "img"),
+    os.path.join(os.path.dirname(__file__), "static", "icons")
 )
 # List of finder classes that know how to find static files in
 # various locations.

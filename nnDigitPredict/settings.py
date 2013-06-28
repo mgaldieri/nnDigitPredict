7dda3bcea7c566bf376cb6fbc423f3260e066c43
Tiago Barufi <barufi@gmail.com>
2013-06-28 12:08:02 -0300

    home is done

--- nnDigitPredict/settings.py
+++ nnDigitPredict/settings.py
@@ -63,7 +63,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/home/tiago/works/nndigitpredict/static' #os.path.join(os.path.dirname(__file__), 'static').replace('\\', '/')
+STATIC_ROOT = '/home/tiago/works/nndigitpredict/' #os.path.join(os.path.dirname(__file__), 'static').replace('\\', '/')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -71,12 +71,11 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    #os.path.join(os.path.dirname(__file__), 'static').replace('\\', '/'),
+    ("static", os.path.join(os.path.dirname(__file__), "static")),
 )
-
+STATIC_DIRS = (
+    ("static", os.path.join(os.path.dirname(__file__), "static")),
+)
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
@@ -84,6 +83,7 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
+
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'x*3@xly^tkq(h$1_54_vn#83_(j@c$yl$legovq1#565%*h&u*'
